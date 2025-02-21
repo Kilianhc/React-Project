97b24203a66fcb7affdcf3cf2f13dd4a8fa507bb
Kilian H <kilianhc@hotmail.com>
2025-02-21 09:35:11 +0000

    v.1.3 (Delete button only for admin)

--- src/Components/MovieCard.jsx
+++ src/Components/MovieCard.jsx
@@ -1,9 +1,16 @@
 import { Card, CardMedia, CardContent, Typography, Button, Stack } from "@mui/material";
-import { Link } from "react-router-dom";
+import { Link, useLocation } from "react-router-dom";
 import VisibilityIcon from "@mui/icons-material/Visibility";
 import DeleteIcon from "@mui/icons-material/Delete";
 
 export default function MovieCard({ movie, onRemove, isAuthenticated }) {
+
+    const location = useLocation()
+    const isDashboard = location.pathname === "/"
+    const isFavorite = location.pathname === "/favorites"
+    const isWatched = location.pathname === "/watched"
+    const isWatchList = location.pathname === "/watchlist"
+
     return (
         <Card sx={{
             bgcolor: "black", margin: "auto", width: { xs: "85%", sm: "320px" }, height: "400px", borderRadius: "15px", border: "5px solid", borderColor: "primary.main",
@@ -16,18 +23,18 @@ export default function MovieCard({ movie, onRemove, isAuthenticated }) {
         }}>
             <CardMedia component="img" height="300" width="300" image={movie.image} alt={movie.title} sx={{ width: "100%", height: "250px", objectFit: "cover" }} />
             <CardContent >
-                <Typography variant="h6" sx={{ pb: "15px", color: "white" }}>
+                <Typography variant="h6" sx={{ pb: "15px", color: "white", textAlign:"center" }}>
                     {movie.title} ({movie.year})
                 </Typography>
                 <Stack sx={{ bgcolor: "black", justifyContent: "center" }} direction="row" spacing={2}>
                     <Button component={Link} to={`/movie/${movie.id}`} variant="contained" sx={{ textAlign: "center", height: "35px", bgcolor: "background.main", "&:hover": { transform: "scale(1.05)" } }}
                         startIcon={<VisibilityIcon />}> Detalles
                     </Button>
-                    {/* {isAuthenticated && ( */}
+                    {(isFavorite || isWatchList || isWatched || isAuthenticated && isDashboard) && (
                         <Button variant="contained" sx={{ bgcolor: "accent.main", height: "35px", "&:hover": { transform: "scale(1.05)" } }} onClick={() => onRemove(movie.id)}
                             startIcon={<DeleteIcon />}> Eliminar
                         </Button>
-                    {/* )} */}
+                    )}
 
                 </Stack>
             </CardContent>
